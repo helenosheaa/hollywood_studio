f8a5ce8780228e199b5ae2547278899eb57c7e88
helenosheaa <helenosheaa@gmail.com>
2018-07-13 11:43:52 +0100

    subclasses of staff setup

--- src/main/java/models/Actor.java
+++ src/main/java/models/Actor.java
@@ -1,4 +1,30 @@
 package models;
 
-public class Actor {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "actors")
+
+public class Actor extends Staff {
+
+    private List<Film> films;
+
+    public Actor(String firstName, String lastName, double bankAccount) {
+        super(firstName, lastName, bankAccount);
+        this.films = new ArrayList<Film>();
+    }
+
+    public Actor() {
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
--- src/main/java/models/Director.java
+++ src/main/java/models/Director.java
@@ -1,4 +1,30 @@
 package models;
 
-public class Director {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "directors")
+
+public class Director extends Staff {
+
+    private List<Film> films;
+
+    public Director(String firstName, String lastName, double bankAccount) {
+        super(firstName, lastName, bankAccount);
+        this.films = new ArrayList<Film>();
+    }
+
+    public Director() {
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
