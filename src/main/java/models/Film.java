24b1c2b4e335861a3ec39ffb3692366ae1cc6eca
helenosheaa <helenosheaa@gmail.com>
2018-07-13 11:51:25 +0100

    studio & film setup

--- src/main/java/models/Film.java
+++ src/main/java/models/Film.java
@@ -1,4 +1,74 @@
 package models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "films")
+
 public class Film {
+    private int id;
+    private String title;
+    private List<Actor> actors;
+    private Director director;
+    private Studio studio;
+
+    public Film(int id, String title, Director director, Studio studio) {
+        this.id = id;
+        this.title = title;
+        this.actors = new ArrayList<Actor>();
+        this.director = director;
+        this.studio = studio;
+    }
+
+    public Film() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "actor")
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    @Column(name = "director")
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    @Column(name = "studio")
+    public Studio getStudio() {
+        return studio;
+    }
+
+    public void setStudio(Studio studio) {
+        this.studio = studio;
+    }
 }
--- src/main/java/models/Studio.java
+++ src/main/java/models/Studio.java
@@ -1,4 +1,59 @@
 package models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "studios")
 public class Studio {
+    private int id;
+    private String name;
+    private double budget;
+    private List<Film> films;
+
+    public Studio(int id, String name, double budget) {
+        this.id = id;
+        this.name = name;
+        this.budget = budget;
+        this.films = new ArrayList<Film>();
+    }
+
+    public Studio() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
